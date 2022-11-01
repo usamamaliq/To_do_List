c09ca040f34e2f245e7aedd085f15bcfd6a56081
Usama Masood <usamamaliq@gmail.com>
2022-11-01 22:30:14 +0500

    Add task lists dynamically through JS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import _ from 'lodash';
 import './style.css';
-// function component() {
-//     const element = document.createElement('div');
+ const tasks=[
+  {
+    description : "wash the dishes",
+    completed: false,
+    index: 1
+  },
+  {
+    description : "complete the project",
+    completed: false,
+    index: 2
+  },
+ ];
+
+ const taskContainer = document.querySelector('.tasks-cont');
+ for (let i=0; i<tasks.length; i+=1){
   
-//     // Lodash, now imported by this script
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//     element.classList.add('hello');
-//     return element;
-//   }
-  
-  document.body.appendChild(component());
+  taskContainer.innerHTML+= `<div class="task-list">
+  <input class="pointer " type="checkbox" name="wash the dishes" id="${tasks[i].index}">
+  <label  for="${tasks[i].index}">${tasks[i].description}</label>
+  <i class="fa  fa-ellipsis-v menuIcon pointer"></i>
+</div>`;}
